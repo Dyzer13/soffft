354472ca5e25f01b31a37f615c0dcae17f65ad4d
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-17 23:19:29 +0100

    Update bott.js

--- bott.js
+++ bott.js
@@ -14,7 +14,7 @@ client.on('message',async message => {
     let msgCount = 0;
     let errorCount = 0;
     let successCount = 0;
-    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`);
+    message.channel.send(`**- [ :bookmark: :: ${msgCount} ] ・عدد الرسائل المرسلة**\n**- [ :inbox_tray: :: ${successCount} ] ・عدد الرسائل المستلمة**\n**- [ :outbox_tray: :: ${errorCount} ]・عدد الرسائل الغير مستلمة**`)
       .then(msg => {
       message.guild.members.forEach(g => {
         g.send(args.slice(1).join(' ')).then(() => {
