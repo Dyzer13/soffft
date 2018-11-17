0a2c72cb9a8fbbdebbcbf12b0ec77ddd1534d6e7
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-18 00:00:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,22 @@ client.on('message',async message => {
   }
 });
 
+client.on("message", message => { 
+  if (message.content.startsWith(prefix + "bc")) { 
+    if (!message.member.hasPermission("ADMINISTRATOR")) return; 
+  let args = message.content.split(" ").slice(1); 
+   var argresult = args.join(' '); 
+      message.guild.members.filter
+      (m => m.presence.status !== 'all')
+             .forEach(m => { 
+         m.send(`${argresult}\n ${m}`);}) 
+    message.channel.send(`\`${message.guild.members.filter(m => 
+                                                           
+      m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+    
+    message.delete(); 
+  }; 
+}); 
 
 
 
