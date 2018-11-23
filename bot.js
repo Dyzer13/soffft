20f14b057f8baf97db0d98deadf9580321bcb5fc
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-23 11:58:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 console.log('By KBOOOOOOOOSH-YT');
-client.on('ready', () => {
-  console.log(`Logged in as ${LOka.user.tag} !`);
- 
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('$bc-bot')){
-    if(!message.author.id === '500019474494128139') return;
-    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
 
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === 'اي دي صاحب البوت') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);
