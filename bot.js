ab9c167b5652ac6fc6077782e28fae1cc1e3d03b
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-11-23 22:07:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-bc-users')){
-if(!message.author.id === '500019474494128139') return;
+if(!message.author.id === '515474180603641866') return;
 message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
 client.users.forEach(m =>{
 m.sendMessage(args)
